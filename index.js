12d9ff20a8d5baeeb26f3d24753d85e815bab494
Burak Karasoy <burak.karasoy@dia.com.tr>
2017-03-30 14:34:49 +0300

    openfiledialog fonksiyonu bulunamadı hatası

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ var {Intent} = require('react-native').NativeModules;
 
 var DiaFS = {
   startNativeIntent(PACKAGE_NAME, FILE_PATH){
-      return Intent.startActivityForResult(PACKAGE_NAME, FILE_PATH);
+    return Intent.startActivityForResult(PACKAGE_NAME, FILE_PATH);
   },
   pickImage(){
     return Intent.pickImage();
+  },
+  openFileDialog(){
+    return Intent.openFileDialog();
+  },
+  triggerExcelApplications(_path){
+    return Intent.triggerExcelApplications(_path);
   }
 }
 
